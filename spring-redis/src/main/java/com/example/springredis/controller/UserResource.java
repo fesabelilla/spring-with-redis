43be16e1e2b0f71f93b1b5ea8439a2b88569eb49
fesabelilla <fesabelilla@gmail.com>
2022-04-09 17:01:07 +0600

    spring redis simple application | CRUD Operations

---
+++ spring-redis/src/main/java/com/example/springredis/controller/UserResource.java
@@ -0,0 +1,46 @@
+package com.example.springredis.controller;
+
+import com.example.springredis.model.User;
+import com.example.springredis.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/rest/user")
+public class UserResource {
+
+    private UserRepository userRepository;
+
+    public UserResource(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/add/{id}/{name}")
+    public User add(@PathVariable("id") String id,
+                    @PathVariable("name") String name) {
+        userRepository.save(new User(id, name, 20000L));
+        return userRepository.findById(id);
+    }
+
+    @GetMapping("/update/{id}/{name}")
+    public User update(@PathVariable("id") String id,
+                    @PathVariable("name") String name) {
+        userRepository.update(new User(id, name, 25000L));
+        return userRepository.findById(id);
+    }
+
+    @GetMapping("/delete/{id}")
+    public Map<String, User> delete(@PathVariable("id") String id) {
+        userRepository.delete(id);
+        return findAll();
+    }
+
+    @GetMapping("/all")
+    public Map<String, User> findAll() {
+        return userRepository.findAll();
+    }
+}
---
+++ spring-redis/src/main/java/com/example/springredis/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.springredis.repository;
+
+import com.example.springredis.model.User;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserRepository {
+
+    void save(User user);
+
+    Map<String, User> findAll();
+
+    User findById(String id);
+
+    void update(User user);
+
+    void delete(String id);
+}
